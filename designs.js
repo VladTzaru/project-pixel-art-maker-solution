d4e07082cfb4b4e30c0eae97c308031138622897
Vladimir Bojovic <bvanja.87@gmail.com>
2017-09-06 14:36:34 +0200

    Declare all input values

--- designs.js
+++ designs.js
@@ -1,5 +1,17 @@
-// Select color input
-// Select size input
+const pixelModel = {
+  // Select color input
+  selectedColor: $('#colorPicker').val(),
+  // Select size input
+  selectedWidth: $('#input_width').val(),
+  selectedHeight: $('#input_height').val()
+};
+
+console.log(pixelModel.selectedColor, pixelModel.selectedWidth, pixelModel.selectedHeight);
+
+
+const pixelController = {};
+
+const pixelView = {};
 
 // When size is submitted by the user, call makeGrid()
 
