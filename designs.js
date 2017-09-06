e1f0df60de047e4fe02047c2ada5bc2a080d295f
Vladimir Bojovic <bvanja.87@gmail.com>
2017-09-07 01:06:12 +0200

    Added solution

--- designs.js
+++ designs.js
@@ -1,68 +1,44 @@
 $(function() {
-  'use strict';
+    'use strict';
+
+    // DOM pointers
+    const canvasEl  = $('#pixel_canvas');
+    const submitBtn = $('#input_submit');
 
 
-  /*------------ PIXEL MODEL ------------*/
-  const pixelModel = {};
-
-
-  /*------------ PIXEL CONTROLLER ------------*/
-  const pixelController = {
-    init() {pixelView.init();},
-    getColor() {return $('#colorPicker').val()},
-    getWidth() {return $('#input_width').val()},
-    getHeight() {return $('#input_height').val()},
-    setColor() {
-      let colorEl = $('#colorPicker');
-      return colorEl.val();
+    // Set color
+    function setColor() {
+        let colorVal = $('#colorPicker').val();
+        return colorVal;
     }
-  };
 
-  /*------------ PIXEL VIEW ------------*/
-  const pixelView = {
-    init() {
-
-      // All DOM pointers
-      let canvasEl = $('#pixel_canvas');
-      let submitEl = $('#input_submit');
-
-
-      // When size is submitted by the user, call makeGrid()
-      function makeGrid() {
+    // Create grid
+    function makeGrid() {
 
         // Get current input values
-        let width = pixelController.getWidth();
-        let height = pixelController.getHeight();
-
+        let width  = $('#input_width').val();
+        let height = $('#input_height').val();
 
         // Iterate and append newly created elements
         for (let row = 0; row < height; row++) {
-          let trEl = $('<tr></tr>');
+            let trEl = $('<tr></tr>');
 
-          for (let col = 0; col < width; col++) {
-            let tdEl = $('<td></td>');
-            $(trEl).append(tdEl);
+            for (let col = 0; col < width; col++) {
+                let tdEl = $('<td></td>');
+                $(trEl).append(tdEl);
 
-            // Eventlistener that handles background-color of each pixel
-            $(tdEl).click(function() {
-              tdEl.css('background-color', pixelController.setColor());
-            });
-          }
-          $(canvasEl).append(trEl);
+                // Event listener that sets background-color for each pixel
+                $(tdEl).click(function() {
+                    tdEl.css('background-color', setColor());
+                });
+            }
+            $(canvasEl).append(trEl);
         }
+    }
 
-      }
-
-      // Event listener for our submit input
-      $(submitEl).click(function() {
+    // Event listener that invokes makeGrid() function
+    $(submitBtn).click(function() {
         makeGrid();
         return false;
-      });
-
-    }
-  };
-
-
-  /*------------ INVOKE METHODS ------------*/
-  pixelController.init();
+    });
 });
